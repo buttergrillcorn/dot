2e16682a0ae6c62cd7d7c34d073f6bd3aa95bc9d
buttergrillcorn <jamesyuli@outlook.com>
2023-11-22 17:51:08 +0000

    add: Imported .emacs.d

---
+++ dotfiles/emacs.d/init.el
@@ -0,0 +1,9 @@
+;; Hide the splash screen 
+(setq inhibit-startup-message t
+      visible-bell t)
+
+;; UI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-display-line-numbers-mode 1)
