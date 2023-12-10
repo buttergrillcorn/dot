572715cf3adda0821bb3ed3ba98d47515c781d04
buttergrillcorn <jamesyuli@outlook.com>
2023-12-10 01:23:32 +0000

    mod: doom font

--- dotfiles/config/doom/font.el
+++ dotfiles/config/doom/font.el
@@ -1,9 +1,9 @@
 {%@@ if profile == "Aloo-Paratha" @@%}
-(setq doom-font (font-spec :family "Sarasa Term SC Nerd" :size 15 :weight 'Semibold)
-      doom-variable-pitch-font (font-spec :family "Sarasa Term SC Nerd" :size 15)
+(setq doom-font (font-spec :family "Sarasa Term SC Nerd" :size 16 :weight 'Regular)
+      doom-variable-pitch-font (font-spec :family "Sarasa Term SC Nerd" :size 16)
       doom-unicode-font (font-spec :family "Symbols Nerd Font Mono" :size 16))
 {%@@ elif profile == "Aloo-Paratha-Laptop" @@%}
-(setq doom-font (font-spec :family "Sarasa Term SC Nerd" :size 18 :weight 'Semibold)
+(setq doom-font (font-spec :family "Sarasa Term SC Nerd" :size 18 :weight 'Regular)
       doom-variable-pitch-font (font-spec :family "Sarasa Term SC Nerd" :size 18)
       doom-unicode-font (font-spec :family "Symbols Nerd Font Mono" :size 18))
 {%@@ endif @@%}
