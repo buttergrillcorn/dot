47b1899c526308e5eb6232919a19e37bb628e86d
buttergrillcorn <jamesyuli@outlook.com>
2023-12-15 00:03:46 +0000

    new neovim config from scratch

---
+++ dotfiles/config/nvim/lua/plugins/zenbones.lua
@@ -0,0 +1,13 @@
+return {
+    "mcchrish/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    name = "zenbones",
+    lazy = false,
+    pirority = 1000,
+    config = function ()
+      vim.cmd.colorscheme "zenbones"
+    end
+}
